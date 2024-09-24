b08d8253ad84de96e2d26e76ae4d35d7a3ef9bae
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-09-24 20:24:59 -0300

    feat: adicionando a classe Cliente

---
+++ src/models/Cliente.ts
@@ -0,0 +1,10 @@
+class Cliente {
+    public nome: string
+    public email: string
+    constructor(nome: string, email: string) {
+        this.nome = nome
+        this.email = email
+    }
+}
+
+export default Cliente
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import get from "../functions/get"
+import Cliente from "../models/Cliente"
 
 function Home() {
-    const [clientes, setClientes] = useState([])
+    const [clientes, setClientes] = useState<Cliente[]>([])
  
     async function buscarClientes() {
-        const data = await get("http://localhost:4444/clientes")
-        setClientes(data)
+        const data: { nome: string, email: string }[] = await get("http://localhost:4444/clientes")
+        const listaCliente: Cliente[] = []
+        data.forEach(cli => listaCliente.push(new Cliente(cli.nome, cli.email)))
+        setClientes(listaCliente)
     }
 
     return (
@@ -16,7 +19,7 @@ function Home() {
             {clientes.map((cliente, key) => {
                 return (
                     <li key={key}>
-                        sadlksalçdk
+                        {cliente.nome} - {cliente.email}
                     </li>
                 )
             })}
