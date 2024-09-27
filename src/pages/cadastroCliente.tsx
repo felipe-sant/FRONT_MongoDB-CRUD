027b058790445711fa24e3544a81de82c76e4d1e
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-09-27 20:52:41 -0300

    feat: adicionando cadastro de cliente

--- src/pages/cadastroCliente.tsx
+++ src/pages/cadastroCliente.tsx
@@ -2,8 +2,37 @@ import FooterComponent from "../components/FooterComponent"
 import HeaderComponent from "../components/HeaderComponent"
 import css from "../styles/cadastroCliente.module.css"
 import back_arrow from "../images/back_arrow.svg"
+import { useState } from "react"
+import Cliente from "../models/Cliente"
+import inserirCliente from "../functions/inserirCliente"
 
 function CadastroCliente() {
+    const [nome, setNome] = useState("")
+    const [errorNome, setErrorNome] = useState(false)
+    const [email, setEmail] = useState("")
+    const [errorEmail, setErrorEmail] = useState(false)
+
+    function handleNome(event: any) {
+        setNome(event.target.value)
+    }
+
+    function handleEmail(event: any) {
+        setEmail(event.target.value)
+    }
+
+    function verificarErros() {
+        nome == "" ? setErrorNome(true) : setErrorNome(false)
+        email == "" ? setErrorEmail(true) : setErrorEmail(false)
+    }
+
+    function cadastrar() {
+        verificarErros()
+        if (nome == "" || email == "") return
+        const cliente = new Cliente("", nome, email)
+        inserirCliente(cliente)
+        window.location.href = "/"
+    }
+
     return (
         <>
             <HeaderComponent />
@@ -17,22 +46,28 @@ function CadastroCliente() {
                     </div>
                     <div className={css.formulario}>
                         <div className={css.input}>
-                            <label htmlFor="nome">Nome:</label>
+                            <label htmlFor="nome" className={css.required}>Nome</label>
                             <input 
                                 type="text" 
                                 placeholder="Adicione o cliente"
+                                value={nome}
+                                onChange={handleNome}
+                                className={errorNome ? css.error : ""}
                             />
                         </div>
                         <div className={css.input}>
-                            <label htmlFor="email">Email:</label>
+                            <label htmlFor="email" className={css.required}>Email</label>
                             <input 
                                 type="email" 
                                 placeholder="Adicione o email"
+                                value={email}
+                                onChange={handleEmail}
+                                className={errorEmail ? css.error : ""}
                             />
                         </div>
                     </div>
                     <div className={css.rodape}>
-                        <button>CADASTRAR</button>
+                        <button onClick={cadastrar}>CADASTRAR</button>
                     </div>
                 </article>
             </main>
