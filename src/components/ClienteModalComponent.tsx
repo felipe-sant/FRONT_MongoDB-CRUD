6aceba181dc149d779e4adf0777e853ba606e167
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-09-27 22:21:39 -0300

    feat: adicionando funcao de atualizar cliente

--- src/components/ClienteModalComponent.tsx
+++ src/components/ClienteModalComponent.tsx
@@ -2,11 +2,32 @@ import Modal from 'react-bootstrap/Modal';
 import ClienteModalType from '../types/clienteModalType';
 import modal from "../styles/modal.module.css";
 import css from "../styles/clienteModal.module.css";
+import { useState } from 'react';
+import Cliente from '../models/Cliente';
+import atualizarCliente from '../functions/atualizarCliente';
 
 function ClienteModalComponent(props: ClienteModalType) {
     const cliente = props.cliente;
     const show = props.show;
     const handleClose = props.handleClose;
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+
+    function handleNome (event: React.ChangeEvent<HTMLInputElement>) {
+        setNome(event.target.value);
+    }
+
+    function handleEmail (event: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(event.target.value);
+    }
+
+    function salvar() {
+        const nomeCliente = nome === "" ? cliente.nome : nome;
+        const emailCliente = email === "" ? cliente.email : email;
+        atualizarCliente(new Cliente(cliente._id, nomeCliente, emailCliente));
+        window.location.reload();
+    }
 
     return (
         <Modal show={show} onHide={handleClose} className={modal.modal}>
@@ -25,6 +46,8 @@ function ClienteModalComponent(props: ClienteModalType) {
                     <input
                         type="text"
                         placeholder={cliente.nome}
+                        onChange={handleNome}
+                        value={nome}
                     />
                 </div>
                 <div className={css.input}>
@@ -32,12 +55,14 @@ function ClienteModalComponent(props: ClienteModalType) {
                     <input
                         type="email"
                         placeholder={cliente.email}
+                        onChange={handleEmail}
+                        value={email}
                     />
                 </div>
                 <p>Caso não queira alterar, deixe em branco</p>
             </Modal.Body>
             <Modal.Footer className={css.rodape}>
-                <button>Salvar</button>
+                <button onClick={salvar}>Salvar</button>
             </Modal.Footer>
         </Modal>
 
