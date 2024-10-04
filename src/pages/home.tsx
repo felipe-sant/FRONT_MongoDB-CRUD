c433f0c4d46e726c1ed18e7389982809ce05aa16
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-10-04 20:34:14 -0300

    style: adicionando estilo a pagina home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,22 @@
+import FooterComponent from "../components/FooterComponent"
+import HeaderComponent from "../components/HeaderComponent"
+import css from "../styles/home.module.css"
+
 function Page_Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <a href="/clientes">clientes</a>
-            <br />
-            <a href="/livros">livros</a>
-        </div>
+        <>
+            <HeaderComponent />
+            <main>
+                <article className={css.main}>
+                    <h1>Escolha o CRUD</h1>
+                    <section className={css.links}>
+                        <button onClick={() => window.location.href = "/clientes"}>Clientes</button>
+                        <button onClick={() => window.location.href = "/livros"}>Livros</button>
+                    </section>
+                </article>
+            </main>
+            <FooterComponent />
+        </>
     )
 }
 
